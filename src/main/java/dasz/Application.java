553609aba0843adb10483f493887e0d7ddbdff99
dariuszszmycinski <darekszmycinski@wp.pl>
2021-03-16 10:40:31 +0100

    initial commit, http endpoints

---
+++ src/main/java/dasz/Application.java
@@ -0,0 +1,75 @@
+package dasz;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.InetSocketAddress;
+import java.net.URLDecoder;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import static java.util.stream.Collectors.*;
+
+public class Application {
+
+    public static void main(String[] args) throws IOException {
+        int serverPort = 8080;
+        HttpServer server = HttpServer.create(new InetSocketAddress(serverPort), 0);
+        server.createContext("/api/start", (exchange -> {
+
+            if ("GET".equals(exchange.getRequestMethod())) {
+                Map<String, List<String>> params = splitQuery(exchange.getRequestURI().getRawQuery());
+                String noNameText = "no names given";
+                String name = params.getOrDefault("name", List.of(noNameText)).stream().findFirst().orElse(noNameText);
+                String respText = "Names checked: " + name;
+                exchange.sendResponseHeaders(200, respText.getBytes().length);
+                OutputStream output = exchange.getResponseBody();
+                output.write(respText.getBytes());
+                output.flush();
+            } else {
+                exchange.sendResponseHeaders(405, -1);// 405 Method Not Allowed
+            }
+            exchange.close();
+        }));
+
+        server.createContext("/api/tables", (exchange -> {
+
+            if ("GET".equals(exchange.getRequestMethod())) {
+                String respText = "Names tables here";
+                exchange.sendResponseHeaders(200, respText.getBytes().length);
+                OutputStream output = exchange.getResponseBody();
+                output.write(respText.getBytes());
+                output.flush();
+            } else {
+                exchange.sendResponseHeaders(405, -1);// 405 Method Not Allowed
+            }
+            exchange.close();
+        }));
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+
+    public static Map<String, List<String>> splitQuery(String query) {
+        if (query == null || "".equals(query)) {
+            return Collections.emptyMap();
+        }
+
+        return Pattern.compile("&").splitAsStream(query)
+                .map(s -> Arrays.copyOf(s.split("="), 2))
+                .collect(groupingBy(s -> decode(s[0]), mapping(s -> decode(s[1]), toList())));
+
+    }
+
+    private static String decode(final String encoded) {
+        try {
+            return encoded == null ? null : URLDecoder.decode(encoded, "UTF-8");
+        } catch (final UnsupportedEncodingException e) {
+            throw new RuntimeException("UTF-8 is a required encoding", e);
+        }
+    }
+}
